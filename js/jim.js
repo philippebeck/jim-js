997793713e247919508c896d091e12eb5fa79365
Philippe Beck <philippe@philippebeck.net>
2019-07-18 16:43:57 +0200

    Remove blank lines & comments

--- js/jim.js
+++ js/jim.js
@@ -25,7 +25,6 @@
 */
 function getRandomInteger(min, max)
 {
-  // Returns a random integer between the min & the max values
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
@@ -37,13 +36,8 @@ function getRandomInteger(min, max)
 */
 function installEventListener(selector, type, listener)
 {
-  // Creates the element empty
   var element;
-
-  // Selects, gets & stores the first DOM object matching to the selector
   element = document.querySelector(selector);
-
-  // Installs an Event Listener on this DOM object
   element.addEventListener(type, listener);
 }
 
@@ -54,7 +48,6 @@ function installEventListener(selector, type, listener)
  */
 function onClickHelp()
 {
-  // Gets the jim help element to toggle the none class
   document.getElementById("jim-help").classList.toggle("none");
 }
 
@@ -64,121 +57,82 @@ function onClickHelp()
  */
 function onKeydown(event)
 {
-  // Gets the keycode, then stores it
   var keyCode = event.which;
 
-  // Checks which action matches with the current keycode
   switch (keyCode)
   {
     case 65: keyA();
     break;
-
     case 66: keyB();
     break;
-
     case 67: keyC();
     break;
-
     case 68: keyD();
     break;
-
     case 69: keyE();
     break;
-
     case 70: keyF();
     break;
-
     case 71: keyG();
     break;
-
     case 72: keyH();
     break;
-
     case 73: keyI();
     break;
-
     case 74: keyJ();
     break;
-
     case 75: keyK();
     break;
-
     case 76: keyL();
     break;
-
     case 77: keyM();
     break;
-
     case 78: keyN();
     break;
-
     case 79: keyO();
     break;
-
     case 80: keyP();
     break;
-
     case 81: keyQ();
     break;
-
     case 82: keyR();
     break;
-
     case 83: keyS();
     break;
-
     case 84: keyT();
     break;
-
     case 85: keyU();
     break;
-
     case 86: keyV();
     break;
-
     case 87: keyW();
     break;
-
     case 88: keyX();
     break;
-
     case 89: keyY();
     break;
-
     case 90: keyZ();
     break;
-
     case 96: key0();
     break;
-
     case 97: key1();
     break;
-
     case 98: key2();
     break;
-
     case 99: key3();
     break;
-
     case 100: key4();
     break;
-
     case 101: key5();
     break;
-
     case 102: key6();
     break;
-
     case 103: key7();
     break;
-
     case 104: key8();
     break;
-
     case 105: key9();
     break;
-
-    // Gets the jim help element to toggle the none class
     default:
     document.getElementById("jim-help").classList.toggle("none");
   }
@@ -186,12 +140,8 @@ function onKeydown(event)
 
 // ***** 3 - MAIN ***** \\
 
-// Waits the complete loading of the page
 document.addEventListener("DOMContentLoaded", function(e)
 {
-  // Adds listeners on the keyboard
   installEventListener("#access", "keydown", onKeydown);
-
-  // Adds listener on the help button
   installEventListener("#help-btn", "click", onClickHelp);
 });
